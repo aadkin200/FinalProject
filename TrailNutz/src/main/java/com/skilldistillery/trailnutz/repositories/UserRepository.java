a6ab45cbd28eb3ce5ddc93fa76787b1e0d3c369c
Alex Adkins <alex.t.adkins@gmail.com>
2021-08-17 16:27:27 -0400

    added boot configuration

---
+++ TrailNutz/src/main/java/com/skilldistillery/trailnutz/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.trailnutz.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.trailnutz.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
---
+++ TrailNutz/src/main/java/com/skilldistillery/trailnutz/services/UserService.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.trailnutz.services;
+
+public interface UserService {
+
+}
