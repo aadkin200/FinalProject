00c58c02da7c994159f276a6cc4e75845ef581e8
Alex Adkins <alex.t.adkins@gmail.com>
2021-08-17 16:14:22 -0400

    added boot project

---
+++ TrailNutz/src/test/java/com/skilldistillery/trailnutz/TrailNutzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.trailnutz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailNutzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
