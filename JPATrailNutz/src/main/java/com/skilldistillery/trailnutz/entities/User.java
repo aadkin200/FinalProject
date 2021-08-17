54de92cb62f5dcb4e16feb6de5ae15c1d68aed6a
Hopeful89 <brandon.stine89@gmail.com>
2021-08-17 15:28:46 -0600

    Test user

--- JPATrailNutz/src/main/java/com/skilldistillery/trailnutz/entities/User.java
+++ JPATrailNutz/src/main/java/com/skilldistillery/trailnutz/entities/User.java
@@ -1,5 +1,34 @@
 package com.skilldistillery.trailnutz.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	
+	public User() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
