a881ff530e7c9276043146333c4558821e24ba3f
Kim Nettleton <kim@Brucebs-MacBook-Pro.local>
2021-08-18 11:33:57 -0600

    Route Type Amenity and Difficulty entities and JUnit Tests

---
+++ JPATrailNutz/src/test/java/com/skilldistillery/trailnutz/entities/RouteTypeTest.java
@@ -0,0 +1,55 @@
+package com.skilldistillery.trailnutz.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RouteTypeTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private RouteType routeType;
+	
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPATrailNutz");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf = null;
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		routeType = em.find(RouteType.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		routeType = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(routeType);
+		assertEquals("Loop", routeType.getName());
+	}
+
+
+	
+
+}
