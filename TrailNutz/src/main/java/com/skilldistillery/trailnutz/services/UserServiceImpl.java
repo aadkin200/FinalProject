f6a475914cb6083054df512d1fd89683b5b4e175
Hopeful89 <brandon.stine89@gmail.com>
2021-08-19 09:52:32 -0600

    admin able to disabled user

--- TrailNutz/src/main/java/com/skilldistillery/trailnutz/services/UserServiceImpl.java
+++ TrailNutz/src/main/java/com/skilldistillery/trailnutz/services/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,6 +16,8 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private UserRepository userRepo;
+	@Autowired
+	PasswordEncoder encoder;
 
 	@Override
 	public User userByUsername(String username) {
@@ -23,27 +26,40 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public User updateUser(User user, String username) {
-		User managedUser = userRepo.findByUsername(username);
-		if(user.getId() == managedUser.getId()) {
-			managedUser.setFavoriteTrailFood(user.getFavoriteTrailFood());
-			managedUser.setImageUrl(user.getImageUrl());
-			managedUser.setFirstName(user.getFirstName());
-			managedUser.setLastName(user.getLastName());
-			userRepo.saveAndFlush(managedUser);
+		User managedUser;
+		try {
+			managedUser = userRepo.findByUsername(username);
+			if(user.getId() == managedUser.getId()) {
+				managedUser.setFavoriteTrailFood(user.getFavoriteTrailFood());
+				managedUser.setImageUrl(user.getImageUrl());
+				managedUser.setFirstName(user.getFirstName());
+				managedUser.setLastName(user.getLastName());
+				if(!encoder.matches(user.getPassword(), managedUser.getPassword())) {
+					managedUser.setPassword(encoder.encode(user.getPassword()));
+				}
+				
+				userRepo.saveAndFlush(managedUser);
+			}
+		} catch (Exception e) {
+			managedUser = null;
 		}
 		return managedUser;
 	}
 
 	@Override
 	public boolean disableUser(int userId, String username) {
-		User admin = userRepo.findByUsername(username);
-		if(admin.getRole().equals("admin")) {
-			Optional<User> user = userRepo.findById(userId);
-			if(user.isPresent()) {
-				user.get().setEnabled(false);
-				userRepo.saveAndFlush(user.get());
-				return true;
+		try {
+			User admin = userRepo.findByUsername(username);
+			if(admin.getRole().equals("admin")) {
+				Optional<User> user = userRepo.findById(userId);
+				if(user.isPresent()) {
+					user.get().setEnabled(false);
+					userRepo.saveAndFlush(user.get());
+					return true;
+				}
 			}
+		} catch (Exception e) {
+			return false;
 		}
 		return false;
 	}
