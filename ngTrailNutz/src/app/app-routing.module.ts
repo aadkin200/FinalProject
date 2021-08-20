2b458ad57a1de08726bb314fd1356f02d104b1dc
Hopeful89 <brandon.stine89@gmail.com>
2021-08-20 09:10:03 -0600

    init routing

--- ngTrailNutz/src/app/app-routing.module.ts
+++ ngTrailNutz/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  // { path: 'login', component: LoginComponent },
+  // { path: '**', component: NotFoundComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
