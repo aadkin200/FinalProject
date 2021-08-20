6ad4355a9a800ff57b08581a754e7b1b7389769f
Alex Adkins <alex.t.adkins@gmail.com>
2021-08-20 12:19:22 -0400

    trail model & service stubbed out

---
+++ ngTrailNutz/src/app/models/trail.ts
@@ -0,0 +1,83 @@
+import { User } from "./user";
+
+export class Trail {
+  id: number;
+  name: string;
+  elevationChangeFeet: number;
+  trailheadLatitude: string;
+  trailheadLongitude: string;
+  city: string;
+  state: string;
+  hazards: string;
+  wildlife: string;
+  details: string;
+  distanceMiles: number;
+  createdAt: string;
+  updatedAt: string;
+  enabled: boolean;
+  approved: boolean;
+
+  users: User[];
+  user: User;
+  difficulty: Difficulty;
+  routeType: RouteType;
+  trailImages: TrailImages[];
+  trailResource: TrailResource[];
+  comments: Comment[];
+  amenities: Amenity[];
+
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    elevationChangeFeet: number = 0,
+    trailheadLatitude: string = '',
+    trailheadLongitude: string = '',
+    city: string = '',
+    state: string = '',
+    hazards: string = '',
+    wildlife: string = '',
+    details: string = '',
+    distanceMiles: number = 0,
+    createdAt: string = '',
+    updatedAt: string = '',
+    enabled: boolean = true,
+    approved: boolean = true,
+
+
+    users: User[] = [],
+    user: User = new User(),
+    difficulty: Difficulty = new Difficulty(),
+    routeType: RouteType = new RouteType(),
+    trailImages: TrailImage[] = [],
+    trailResource: TrailResource[] = [],
+    comments: Comment[] = [],
+    amenities: Amenity[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.elevationChangeFeet = elevationChangeFeet;
+    this.trailheadLatitude = trailheadLatitude;
+    this.trailheadLongitude = trailheadLongitude;
+    this.city = city;
+    this.state = state;
+    this.hazards = hazards;
+    this.wildlife = wildlife;
+    this.details = details;
+    this.distanceMiles = distanceMiles;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.enabled = enabled;
+    this.approved = approved;
+
+    this.users = users;
+    this.user = user;
+    this.difficulty = difficulty;
+    this.routeType = routeType;
+    this.trailImages = trailImages;
+    this.trailResource = trailResource;
+    this.comments = comments;
+    this.amenities = amenities;
+  }
+
+}
---
+++ ngTrailNutz/src/app/services/trail.service.ts
@@ -0,0 +1,60 @@
+import { DatePipe } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Trail } from '../models/trail';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrailService {
+
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient,
+              private datePipe: DatePipe,
+              private auth: AuthService
+    ) { }
+
+  index(): Observable<Trail[]> {
+    return this.http.get<Trail[]>(this.baseUrl, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.error('rideService.index() error');
+        return throwError(err);
+      })
+    );
+  }
+
+  public show(trailId: any) {
+    return this.http.get<Trail>(`${this.baseUrl}/${trailId}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('error in trail.service.ts show()');
+      })
+    );
+  }
+
+  public create(trail: Trail) {
+    return this.http.post<Trail>(this.baseUrl, trail, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('error in trail.service.ts create()');
+      })
+    )
+  }
+
+  getHttpOptions() {
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'X-Requested-with': 'XMLHttpRequest',
+        'Authorization': `Basic ${credentials}`
+      }),
+    };
+    return httpOptions
+  }
+}
