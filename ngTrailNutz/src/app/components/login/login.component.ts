65bcc0fe6783b4e3ec6a3fa2f88871f59c02a724
Alex Adkins <alex.t.adkins@gmail.com>
2021-08-20 13:57:50 -0400

    login html & ts stubbed

--- ngTrailNutz/src/app/components/login/login.component.ts
+++ ngTrailNutz/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginUser: User = new User();
+
+  constructor(private auth: AuthService,
+              private router: Router
+              ) { }
 
   ngOnInit(): void {
+  }
+
+  login(user: User) {
+    this.auth.login(user.username, user.password).subscribe(
+      loggedIn => {
+        console.log('LoginComponent.login(): user logged in');
+        this.router.navigateByUrl('/trail');
+      },
+      fial => {
+        console.error("error in login.component.ts login()");
+      }
+    )
   }
 
 }
