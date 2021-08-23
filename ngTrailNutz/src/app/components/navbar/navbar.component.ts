51d6493f71f08fdcecaf577c3a25d6031a86da38
Alex Adkins <alex.t.adkins@gmail.com>
2021-08-23 19:36:24 -0400

    login screen stubbed

--- ngTrailNutz/src/app/components/navbar/navbar.component.ts
+++ ngTrailNutz/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -25,7 +26,6 @@ export class NavbarComponent implements OnInit {
   }
 
   isAdmin(){
-
 
   }
 
