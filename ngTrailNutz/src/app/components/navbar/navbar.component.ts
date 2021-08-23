3e316b41e289e84c27b88de9fadaa81cf19292df
ren09 <rachel.richardson915@gmail.com>
2021-08-23 16:53:23 -0600

    add navbar to home/profile/feed view

--- ngTrailNutz/src/app/components/navbar/navbar.component.ts
+++ ngTrailNutz/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +11,23 @@ import { AuthService } from 'src/app/services/auth.service';
 export class NavbarComponent implements OnInit {
 
   user: User = new User();
+  admin: boolean = false;
 
-  constructor(private auth: AuthService) { }
+
+  constructor(private auth: AuthService, private userSvc: UserService) { }
 
   ngOnInit(): void {
+      this.userSvc.getUser().subscribe(
+        data => {
+          this.user = data;
+          this.admin = this.isAdmin();
+          console.log(this.admin);
+
+        },
+        err => {
+          console.log("Error retreiving user from service")
+        }
+      );
   }
 
   loggedIn(): boolean {
@@ -21,14 +35,21 @@ export class NavbarComponent implements OnInit {
   }
 
   logout() {
+    this.admin = false;
+    this.user = new User();
     this.auth.logout();
-  }
-
-  isAdmin(){
-
 
   }
 
+  isAdmin(): boolean{
+    // console.log(this.user.role);
+    if (this.user.role === "ADMIN") {
+      return true;
 
-
+    } else {
+      return false;
+    }
+  }
 }
+
+
